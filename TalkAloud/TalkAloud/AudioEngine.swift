33830cbfd4ec51b31c24e2791137ff0eb75486b4
JB184351 <justinbengtson@icloud.com>
2020-03-04 20:23:05 -0700

    Added Audio Engine class but couldn't get the app to run as before

---
+++ TalkAloud/TalkAloud/AudioEngine.swift
@@ -0,0 +1,64 @@
+//
+//  AudioEngine.swift
+//  TalkAloud
+//
+//  Created by Justin Bengtson on 3/4/20.
+//  Copyright © 2020 Justin Bengtson. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+// Struct will make it so I don't have to intialize variables
+// until I need to
+class AudioEngine {
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    var recordingSession: AVAudioSession!
+    var fileName = "selftalkfile.m4a"
+    
+    func setupRecorder() {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        let audioFilename = documentsDirectory.appendingPathComponent(self.fileName)
+        
+        let settings = [AVFormatIDKey: Int(kAudioFormatAppleLossless), AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue, AVEncoderBitRateKey : 320000, AVNumberOfChannelsKey: 2, AVSampleRateKey: 44100.0] as [String: Any]
+        
+        var error: NSError?
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+        } catch {
+            audioRecorder = nil
+        }
+        
+        if let err = error {
+            print("AVAudioRecorder error: \(err.localizedDescription)")
+        } else {
+            audioRecorder.delegate = (self as! AVAudioRecorderDelegate)
+            audioRecorder.prepareToRecord()
+        }
+    }
+    
+    func preparePlayer() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: getFileURL())
+            audioPlayer.delegate = (self as! AVAudioPlayerDelegate)
+            audioPlayer.volume = 1.0
+        } catch {
+            if let err = error as Error? {
+                print("AVAudioPlayer error: \(err.localizedDescription)")
+                audioPlayer = nil
+            }
+        }
+    }
+    
+    func getFileURL() -> URL {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documenetDirectory = urls[0] as URL
+        let soundURL = documenetDirectory.appendingPathComponent(self.fileName)
+        return soundURL
+    }
+}
