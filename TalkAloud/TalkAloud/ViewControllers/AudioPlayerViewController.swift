2aa761c24891a5ba1a4505a94206da89db3b0e2f
JB184351 <justinbengtson@icloud.com>
2020-05-09 16:08:00 -0600

    Changed the way the time is displayed so on reset it doesn't look odd.

--- TalkAloud/TalkAloud/ViewControllers/AudioPlayerViewController.swift
+++ TalkAloud/TalkAloud/ViewControllers/AudioPlayerViewController.swift
@@ -101,7 +101,7 @@ class AudioPlayerViewController: UIViewController, AudioEngineStateChangeDelegat
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
         
-        return String(format: "%02i:%02i", minutes, seconds)
+        return String(format: "%2i:%02i", minutes, seconds)
     }
     
     private func setupSlider() {
