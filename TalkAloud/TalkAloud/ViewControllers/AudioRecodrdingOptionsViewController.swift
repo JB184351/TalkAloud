64d71f8d73662c6434a4379a728c862f84038a82
JB184351 <justinbengtson@icloud.com>
2020-08-18 18:40:46 -0600

    Added a new ViewController and modified custom cell to have a button on each cell.

---
+++ TalkAloud/TalkAloud/ViewControllers/AudioRecodrdingOptionsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AudioRecodrdingOptionsViewController.swift
+//  TalkAloud
+//
+//  Created by Justin Bengtson on 8/17/20.
+//  Copyright © 2020 Justin Bengtson. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecodrdingOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
