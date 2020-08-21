1d3fda99ecb196f46e7d6722dea18e42cba49533
JB184351 <justinbengtson@icloud.com>
2020-08-20 18:53:22 -0600

    Added hardcoded rows for moreOptions TableView

--- TalkAloud/TalkAloud/ViewControllers/MoreOptionsViewConroller.swift
+++ TalkAloud/TalkAloud/ViewControllers/MoreOptionsViewConroller.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
+struct MoreOptionsModel {
+    var title: String?
+    var icon: String?
+    
+    var action: () -> Void
+}
+
 class MoreOptionsViewConroller: UIViewController {
 
+    @IBOutlet var moreOptionsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        moreOptionsTableView.dataSource = self
     }
+    
+    // ViewController should be driven by a model with name and icons
 
+}
+
+extension MoreOptionsViewConroller: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1 Rename
+        // 2 Add Tag
+        // 3 Share
+        // 4 Delete
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        
+        switch row {
+        case 0:
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Rename Cell"
+            return cell
+        case 1:
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Add Tag"
+            return cell
+        case 2:
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Share"
+            return cell
+        case 3:
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Delete"
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    
 }
